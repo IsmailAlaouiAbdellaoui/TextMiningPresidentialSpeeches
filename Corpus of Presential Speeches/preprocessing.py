806f33619aeba380215033d35937de280110b406
Ismail Alaoui Abdellaoui <60974@aui.ma>
2019-05-12 17:20:36 +0200

    some flair testing

--- Corpus of Presential Speeches/preprocessing.py
+++ Corpus of Presential Speeches/preprocessing.py
@@ -243,170 +243,170 @@ test_loader = DataLoader(test_data, shuffle=True, batch_size=batch_size)
 
 dataiter = iter(train_loader)
 sample_x, sample_y = dataiter.next()
-print('Sample input size: ', sample_x.size()) # batch_size, seq_length
-print('Sample input: \n', sample_x)
-print()
-print('Sample label size: ', sample_y.size()) # batch_size
-print('Sample label: \n', sample_y)
+#print('Sample input size: ', sample_x.size()) # batch_size, seq_length
+#print('Sample input: \n', sample_x)
+#print()
+#print('Sample label size: ', sample_y.size()) # batch_size
+#print('Sample label: \n', sample_y)
 
-import torch.nn as nn
-
-class SentimentLSTM(nn.Module):
-    """
-    The RNN model that will be used to perform Sentiment analysis.
-    """
-
-    def __init__(self, vocab_size, output_size, embedding_dim, hidden_dim, n_layers, drop_prob=0.5):
-        """
-        Initialize the model by setting up the layers.
-        """
-        super().__init__()
-
-        self.output_size = output_size
-        self.n_layers = n_layers
-        self.hidden_dim = hidden_dim
-        
-        # embedding and LSTM layers
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, 
-                            dropout=drop_prob, batch_first=True)
-        
-        # dropout layer
-        self.dropout = nn.Dropout(0.3)
-        
-        # linear and sigmoid layers
-        self.fc = nn.Linear(hidden_dim, output_size)
-        self.sig = nn.Sigmoid()
-        
-
-    def forward(self, x, hidden):
-        """
-        Perform a forward pass of our model on some input and hidden state.
-        """
-        batch_size = x.size(0)
-
-        # embeddings and lstm_out
-        embeds = self.embedding(x)
-        lstm_out, hidden = self.lstm(embeds, hidden)
-    
-        # stack up lstm outputs
-        lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
-        
-        # dropout and fully-connected layer
-        out = self.dropout(lstm_out)
-        out = self.fc(out)
-        # sigmoid function
-        sig_out = self.sig(out)
-        
-        # reshape to be batch_size first
-        sig_out = sig_out.view(batch_size, -1)
-        sig_out = sig_out[:, -1] # get last batch of labels
-        
-        # return last sigmoid output and hidden state
-        return sig_out, hidden
-    
-    
-    def init_hidden(self, batch_size):
-        ''' Initializes hidden state '''
-        # Create two new tensors with sizes n_layers x batch_size x hidden_dim,
-        # initialized to zero, for hidden state and cell state of LSTM
-        weight = next(self.parameters()).data
-        
-#        if (train_on_gpu):
-#            hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda(),
-#                  weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda())
-#        else:
-        hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_(),
-                  weight.new(self.n_layers, batch_size, self.hidden_dim).zero_())
-        
-        return hidden
-
-
-# Instantiate the model w/ hyperparams
-vocab_size = len(vocab_to_int)+1 # +1 for the 0 padding
-output_size = 1
-embedding_dim = 400
-hidden_dim = 256
-n_layers = 2
-net = SentimentLSTM(vocab_size, output_size, embedding_dim, hidden_dim, n_layers)
-print(net)
-
-# loss and optimization functions
-lr=0.001
-
-criterion = nn.BCELoss()
-optimizer = torch.optim.Adam(net.parameters(), lr=lr)
-
-
-# training params
-
-epochs = 4 # 3-4 is approx where I noticed the validation loss stop decreasing
-
-counter = 0
-print_every = 100
-clip=5 # gradient clipping
-
-# move model to GPU, if available
-#if(train_on_gpu):
-#    net.cuda()
-
-net.train()
-# train for some number of epochs
-for e in range(epochs):
-    # initialize hidden state
-    h = net.init_hidden(batch_size)
-
-    # batch loop
-    for inputs, labels in train_loader:
-        counter += 1
-
-#        if(train_on_gpu):
-#            inputs, labels = inputs.cuda(), labels.cuda()
-
-        # Creating new variables for the hidden state, otherwise
-        # we'd backprop through the entire training history
-        h = tuple([each.data for each in h])
-
-        # zero accumulated gradients
-        net.zero_grad()
-
-        # get the output from the model
-        inputs = inputs.type(torch.LongTensor)
-        output, h = net(inputs, h)
-
-        # calculate the loss and perform backprop
-        loss = criterion(output.squeeze(), labels.float())
-        loss.backward()
-        # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
-        nn.utils.clip_grad_norm_(net.parameters(), clip)
-        optimizer.step()
-
-        # loss stats
-        if counter % print_every == 0:
-            # Get validation loss
-            val_h = net.init_hidden(batch_size)
-            val_losses = []
-            net.eval()
-            for inputs, labels in valid_loader:
-
-                # Creating new variables for the hidden state, otherwise
-                # we'd backprop through the entire training history
-                val_h = tuple([each.data for each in val_h])
-
-#                if(train_on_gpu):
-#                    inputs, labels = inputs.cuda(), labels.cuda()
-
-                inputs = inputs.type(torch.LongTensor)
-                output, val_h = net(inputs, val_h)
-                val_loss = criterion(output.squeeze(), labels.float())
-
-                val_losses.append(val_loss.item())
-
-            net.train()
-            print("Epoch: {}/{}...".format(e+1, epochs),
-                  "Step: {}...".format(counter),
-                  "Loss: {:.6f}...".format(loss.item()),
-                  "Val Loss: {:.6f}".format(np.mean(val_losses)))
+#import torch.nn as nn
+#
+#class SentimentLSTM(nn.Module):
+#    """
+#    The RNN model that will be used to perform Sentiment analysis.
+#    """
+#
+#    def __init__(self, vocab_size, output_size, embedding_dim, hidden_dim, n_layers, drop_prob=0.5):
+#        """
+#        Initialize the model by setting up the layers.
+#        """
+#        super().__init__()
+#
+#        self.output_size = output_size
+#        self.n_layers = n_layers
+#        self.hidden_dim = hidden_dim
+#        
+#        # embedding and LSTM layers
+#        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+#        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, 
+#                            dropout=drop_prob, batch_first=True)
+#        
+#        # dropout layer
+#        self.dropout = nn.Dropout(0.3)
+#        
+#        # linear and sigmoid layers
+#        self.fc = nn.Linear(hidden_dim, output_size)
+#        self.sig = nn.Sigmoid()
+#        
+#
+#    def forward(self, x, hidden):
+#        """
+#        Perform a forward pass of our model on some input and hidden state.
+#        """
+#        batch_size = x.size(0)
+#
+#        # embeddings and lstm_out
+#        embeds = self.embedding(x)
+#        lstm_out, hidden = self.lstm(embeds, hidden)
+#    
+#        # stack up lstm outputs
+#        lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
+#        
+#        # dropout and fully-connected layer
+#        out = self.dropout(lstm_out)
+#        out = self.fc(out)
+#        # sigmoid function
+#        sig_out = self.sig(out)
+#        
+#        # reshape to be batch_size first
+#        sig_out = sig_out.view(batch_size, -1)
+#        sig_out = sig_out[:, -1] # get last batch of labels
+#        
+#        # return last sigmoid output and hidden state
+#        return sig_out, hidden
+#    
+#    
+#    def init_hidden(self, batch_size):
+#        ''' Initializes hidden state '''
+#        # Create two new tensors with sizes n_layers x batch_size x hidden_dim,
+#        # initialized to zero, for hidden state and cell state of LSTM
+#        weight = next(self.parameters()).data
+#        
+##        if (train_on_gpu):
+##            hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda(),
+##                  weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda())
+##        else:
+#        hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_(),
+#                  weight.new(self.n_layers, batch_size, self.hidden_dim).zero_())
+#        
+#        return hidden
+#
+#
+## Instantiate the model w/ hyperparams
+#vocab_size = len(vocab_to_int)+1 # +1 for the 0 padding
+#output_size = 1
+#embedding_dim = 400
+#hidden_dim = 256
+#n_layers = 2
+#net = SentimentLSTM(vocab_size, output_size, embedding_dim, hidden_dim, n_layers)
+#print(net)
+#
+## loss and optimization functions
+#lr=0.001
+#
+#criterion = nn.BCELoss()
+#optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+#
+#
+## training params
+#
+#epochs = 4 # 3-4 is approx where I noticed the validation loss stop decreasing
+#
+#counter = 0
+#print_every = 100
+#clip=5 # gradient clipping
+#
+## move model to GPU, if available
+##if(train_on_gpu):
+##    net.cuda()
+#
+#net.train()
+## train for some number of epochs
+#for e in range(epochs):
+#    # initialize hidden state
+#    h = net.init_hidden(batch_size)
+#
+#    # batch loop
+#    for inputs, labels in train_loader:
+#        counter += 1
+#
+##        if(train_on_gpu):
+##            inputs, labels = inputs.cuda(), labels.cuda()
+#
+#        # Creating new variables for the hidden state, otherwise
+#        # we'd backprop through the entire training history
+#        h = tuple([each.data for each in h])
+#
+#        # zero accumulated gradients
+#        net.zero_grad()
+#
+#        # get the output from the model
+#        inputs = inputs.type(torch.LongTensor)
+#        output, h = net(inputs, h)
+#
+#        # calculate the loss and perform backprop
+#        loss = criterion(output.squeeze(), labels.float())
+#        loss.backward()
+#        # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+#        nn.utils.clip_grad_norm_(net.parameters(), clip)
+#        optimizer.step()
+#
+#        # loss stats
+#        if counter % print_every == 0:
+#            # Get validation loss
+#            val_h = net.init_hidden(batch_size)
+#            val_losses = []
+#            net.eval()
+#            for inputs, labels in valid_loader:
+#
+#                # Creating new variables for the hidden state, otherwise
+#                # we'd backprop through the entire training history
+#                val_h = tuple([each.data for each in val_h])
+#
+##                if(train_on_gpu):
+##                    inputs, labels = inputs.cuda(), labels.cuda()
+#
+#                inputs = inputs.type(torch.LongTensor)
+#                output, val_h = net(inputs, val_h)
+#                val_loss = criterion(output.squeeze(), labels.float())
+#
+#                val_losses.append(val_loss.item())
+#
+#            net.train()
+#            print("Epoch: {}/{}...".format(e+1, epochs),
+#                  "Step: {}...".format(counter),
+#                  "Loss: {:.6f}...".format(loss.item()),
+#                  "Val Loss: {:.6f}".format(np.mean(val_losses)))
 
 #def word2vecnegatives(stemmed_words_negatives):
     
@@ -416,8 +416,51 @@ for e in range(epochs):
 #-Create a big list of scores ( for each word)
 #-Training SVM (or LSTM) using this newly created dataset
 #-Use trained model to test on our speeches
-    
+from flair.embeddings import WordEmbeddings
 
+glove_embedding = WordEmbeddings('glove')
+from flair.data import Sentence
+sentence = Sentence('The grass is green .')
+
+glove_embedding.embed(sentence)
+
+#for token in sentence:
+#    print(token)
+#    print(token.embedding)
+    
+#from flair.embeddings import FlairEmbeddings
+#
+## init embedding
+#flair_embedding_forward = FlairEmbeddings('news-forward')
+#
+## create a sentence
+#sentence = Sentence('The grass is green .')
+#
+## embed words in sentence
+#flair_embedding_forward.embed(sentence)
+##print(str(sentence.embedding.))
+#print(sentence.get_embedding())
+import torch
+from flair.data import Sentence
+from flair.embeddings import WordEmbeddings
+
+# load word embeddings
+embeddings = WordEmbeddings('glove')
+
+# some example sentence
+sentence = Sentence('I love Berlin')
+
+# embed sentences
+embeddings.embed(sentence)
+
+
+
+# make one tensor of all word embeddings of a sentence
+sentence_tensor = torch.cat([token.embedding.unsqueeze(0) for token in sentence], dim=0)
+
+# print tensor shape
+print(sentence_tensor.shape) 
+print(sentence_tensor)
             
 
     
